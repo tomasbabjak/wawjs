91374079172fa9c42b38f50c5cb55ad0fce882f2
tomasbabjak <32101724+tomasbabjak@users.noreply.github.com>
2019-12-11 00:48:57 +0100

    Uloha2 v0.0.1

--- 10-cvicenie/uloha02/src/bom/bom.0.1.0.js
+++ 10-cvicenie/uloha02/src/bom/bom.0.1.0.js
@@ -45,3 +45,31 @@ class AddBom extends Transform {
         this._buff = null;
     }
 }
+
+class RemoveBom extends Transform {
+
+    constructor() {
+        super();
+        this._bomDone = false;
+        this._buff = [];
+    }
+
+    _transform(chunk, enc, cb) {
+        if (this._bomDone)
+            return cb(null, chunk);
+
+        this._buff.push(chunk);
+        if (bufLength(this._buff) >= 3)
+            this._pushBuffered();
+
+        cb();
+    }
+
+    _pushBuffered() {
+        let chunk = Buffer.concat([...this._buff]);
+        if (hasBom(chunk)) chunk = chunk.slice(3);
+        this.push(chunk);
+        this._bomDone = true;
+        this._buff = null;
+    }
+}
--- 10-cvicenie/uloha02/src/bom/index.js
+++ 10-cvicenie/uloha02/src/bom/index.js
@@ -1,4 +1,1 @@
-//module.exports=require("./bom.0.0.0.js");
-//...
-module.exports=require("./bom.0.0.6.js");
-
+module.exports=require("./bom.0.1.0.js");
